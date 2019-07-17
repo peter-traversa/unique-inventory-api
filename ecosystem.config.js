efbf4a554387b38aff8604508e4872dc01490f93
andigan <andrew.nease.code@gmail.com>
2019-07-17 02:10:50 -0400

    cleanup pm2 deploy file 9

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -26,8 +26,8 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:peter-traversa/unique-inventory-api.git',
       path : '/var/www/unique-inventory-api',
-      'pre-deploy-local' : `rsync -av --progress ./config ${secrets.deployUser}@${secrets.deployUrl}:/var/www/config_to_copy`,
-      'post-deploy' : 'npm install && npx pm2 reload ecosystem.config.js --env production'
+      'pre-deploy-local' : `scp ./config/secrets.js ${secrets.deployUser}@${secrets.deployUrl}:'/var/www/config-to-move/secrets.js'`,
+      'post-deploy' : 'cp /var/www/config-to-move/secrets.js /var/www/unique-inventory-api/config/secrets.js && npm install && npx pm2 reload ecosystem.config.js --env production'
     }
   }
 };
